b644d27619550ee4e5d10731b2363a24903b0726
Simplewebbiz <john@webfitters.com>
2026-06-29 14:56:28 -0500

    Scope central routes to central domains

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,11 +6,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         channels: __DIR__ . '/../routes/channels.php',
         then: function () {
+            $centralDomains = config('tenancy.central_domains', []);
+
+            foreach ($centralDomains as $domain) {
+                \Illuminate\Support\Facades\Route::middleware('web')
+                    ->domain($domain)
+                    ->group(base_path('routes/web.php'));
+            }
+
             \Illuminate\Support\Facades\Route::middleware('web')
                 ->group(base_path('routes/tenant.php'));
         },
@@ -47,5 +54,6 @@ return Application::configure(basePath: dirname(__DIR__))
             ], 404);
         });
     })->create();
+
 
 
